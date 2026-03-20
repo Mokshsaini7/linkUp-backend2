b840bd40e66fb04cb187746cca4e709bc76db8c3
Moksh Saini <mokshpc6829@gmail.com>
2026-03-20 18:46:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -479,4 +479,4 @@ if __name__ == "__main__":
     print("📱  Real Dev  →  http://<YOUR_PC_IP>:5000")
     print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n")
     port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=False
+app.run(host="0.0.0.0", port=port, debug=False)
